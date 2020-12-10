f8b3fe89b54e2c79e6272ef55adc19683cc0cd1b
Thomas Joseph <thomasartin@gmail.com>
2020-12-10 16:07:36 +0200

    Add files via upload

--- server-1.cpp
+++ server-1.cpp
@@ -1,14 +1,14 @@
-// Server side C/C++ program to demonstrate Socket programming 
-#include <unistd.h> 
-#include <stdio.h> 
-#include <sys/socket.h> 
-#include <stdlib.h> 
-#include <netinet/in.h> 
+// Server side C/C++ program to demonstrate Socket programming
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
 #include <string.h>
-#include <iostream> 
-#include "header.h"
+#include <iostream>
+#include "header-1.h"
 #define MAX_SEQ 3
-#define PORT 8080 
+#define PORT 8080
 #define sending_data 0
 #define recieving_Data 1
 int current_state = sending_data;
@@ -31,13 +31,11 @@ static int packet_counter = 0;
 *p = array_of_packets[packet_counter];
 packet_counter =  (packet_counter + 1) % 3  ;
 
-
-
 }
 
 void to_physical_layer(frame* s, int socket){
 
-    send(socket , s->info.data , 50 , 0 ); 
+    send(socket , s->info.data , 50 , 0 );
     physical_buffer.ack = s->ack;
     physical_buffer.info = s->info;
     physical_buffer.kind = s->kind;
@@ -46,7 +44,6 @@ void to_physical_layer(frame* s, int socket){
 }
 
 void start_timer(seq_nr k){
-
 
 }
 
@@ -94,7 +91,7 @@ void disable_network_layer(void){
 
 }
 
-static void send_data(seq_nr frame_nr, seq_nr frame_expected, packet buffer[])
+static void send_data(seq_nr frame_nr, seq_nr frame_expected, packet buffer[],int socket)
 {
     /* Construct and send a data frame. */
     std::cout<<"iam in sending_Data";
@@ -102,7 +99,7 @@ static void send_data(seq_nr frame_nr, seq_nr frame_expected, packet buffer[])
     s.info = buffer[frame_nr]; 				/* insert packet into frame */
     s.seq = frame_nr; 						/* insert sequence number into frame */
     s.ack = (frame_expected + MAX_SEQ) % (MAX_SEQ + 1);	/* piggyback ack */
-    to_physical_layer(&s); 					/* transmit the frame */ 
+    to_physical_layer(&s,socket); 					/* transmit the frame */
     start_timer(frame_nr); 					/* start the timer running */
     std::cout<<"sending packet"<<s.info.data<<std::endl;
 }
@@ -113,14 +110,14 @@ static void send_data(seq_nr frame_nr, seq_nr frame_expected, packet buffer[])
 
 
 
-int main(int argc, char const *argv[]) 
-{ 
-    int server_fd, new_socket, valread; 
-    struct sockaddr_in address; 
-    int opt = 1; 
-    int addrlen = sizeof(address); 
-    //char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    //char buffer[1024] = {0};
+    char *hello = "Hello from server";
     seq_nr next_frame_to_send; 				/* MAX_SEQ > 1; used for outbound stream */
     seq_nr ack_expected; 					/* oldest frame as yet unacknowledged */
     seq_nr frame_expected; 					/* next frame expected on inbound stream */
@@ -136,50 +133,50 @@ int main(int argc, char const *argv[])
     next_frame_to_send = 0; 					/* next frame going out */
     frame_expected = 0; 					/* number of frame expected inbound */
     nbuffered = 0;						/* initially no packets are buffered */
-       
-    // Creating socket file descriptor 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
-    { 
-        perror("socket failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
-    { 
-        perror("setsockopt"); 
-        exit(EXIT_FAILURE); 
-    } 
-    address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
-       
-    // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
-    { 
-        perror("bind failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if (listen(server_fd, 3) < 0) 
-    { 
-        perror("listen"); 
-        exit(EXIT_FAILURE); 
-    } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
-    { 
-        perror("accept"); 
-        exit(EXIT_FAILURE); 
-    } 
-    
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
     while(1){
-    
-    //valread = read( new_socket , buffer, 1024); 
-    //printf("%s\n",buffer ); 
-    //send(new_socket , "hello client,its server" , 50 , 0 ); 
-    
+
+    //valread = read( new_socket , buffer, 1024);
+    //printf("%s\n",buffer );
+    //send(new_socket , "hello client,its server" , 50 , 0 );
+
         std::cout<<event<<std::endl;
         wait_for_event(&event); 					/* four possibilities: see event type above */
         switch(event) {
@@ -187,7 +184,7 @@ int main(int argc, char const *argv[])
             /* Accept, save, and transmit a new frame. */
             from_network_layer(&buffer[next_frame_to_send]); 	/* fetch new packet */
             nbuffered = nbuffered + 1; 				/* expand the sender’s window */
-            send_data(next_frame_to_send,frame_expected, buffer);	/* transmit the frame */
+            send_data(next_frame_to_send,frame_expected, buffer,new_socket);	/* transmit the frame */
             inc(next_frame_to_send); 					/* advance sender’s upper window edge */
         break;
 
@@ -217,7 +214,7 @@ int main(int argc, char const *argv[])
         case timeout: 							/* trouble; retransmit all outstanding frames */
         next_frame_to_send = ack_expected; 				/* start retransmitting here */
         for (i = 1; i <= nbuffered; i++) {
-            send_data(next_frame_to_send, frame_expected, buffer);	/* resend frame */
+            send_data(next_frame_to_send, frame_expected, buffer,new_socket);	/* resend frame */
             inc(next_frame_to_send); 					/* prepare to send the next one */
     }
 }
@@ -227,7 +224,7 @@ int main(int argc, char const *argv[])
     else
     disable_network_layer();
 
-    
+
     }
-    return 0; 
-} 
+    return 0;
+}
