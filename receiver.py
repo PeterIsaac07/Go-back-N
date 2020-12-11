12d7526c0f70259d281cf057f562a31e2b5ea7d9
Thomas Joseph <thomasartin@gmail.com>
2020-12-11 14:33:58 +0200

    Add files via upload

---
+++ receiver.py
@@ -0,0 +1,45 @@
+import time, socket, sys, pickle
+import random
+
+
+s = socket.socket()
+host = '127.0.0.1'
+port = 1122
+
+s.connect((host, port))
+print("Connected...\n")
+
+
+while True:
+
+    m = s.recv(1024)
+    m = m.decode()
+    k = s.recv(1024)
+    k = k.decode()
+    k = int(k)
+    i = 0
+    a = ""
+    Ack = ""
+    f = random.randint(0, 1)
+    message = ""
+    while i != k:
+
+        f = random.randint(0, 1)
+        f = 1
+        if (f == 0):
+            Ack = "ACK Lost"
+            message = s.recv(1024)
+            message = message.decode()
+            s.send(Ack.encode())
+
+        elif (f == 1):
+            Ack = "ACK " + str(i)
+            message = s.recv(1024)
+            message = message.decode()
+
+            s.send(Ack.encode())
+            a = a + message
+            i = i + 1
+    print(a)
+
+    print("The message received is :", m)
